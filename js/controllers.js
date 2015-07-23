ae20019da06f672d4c309ef4026001316ec7dce6
Turash Jurij <yrik.turash@gmail.com>
2015-07-23 20:43:30 +0300

    code beautify

--- js/controllers.js
+++ js/controllers.js
@@ -1,22 +1,19 @@
+//-------------- register module -----------------------------//
 var app = angular.module('app', ['ngResource']);
 
-
-app.factory('imagesResource', function ($resource) {
-
+//-------------- service for $resource -----------------------------//
+app.factory('imagesResource', function($resource) {
     return $resource('http://jsonplaceholder.typicode.com/photos/', {
-        
     }, {
         get: {
             method: 'get',
             isArray: true
         }
-
     });
 });
 
-//factory style, more involved but more sophisticated
+//-------------- service for $http -----------------------------//
 app.factory('imagestHttp', function($http) {
-
     return {
         getAlbums: function() {
             return $http.get('http://jsonplaceholder.typicode.com/photos');
@@ -24,47 +21,42 @@ app.factory('imagestHttp', function($http) {
     };
 });
 
-
+//-------------- main controller -----------------------------//
 app.controller('WebService', function($http, imagestHttp, imagesResource) {
-	var vm = this;
+    var vm = this;
 
-	getAlbumsSuccess =
-		function(response) {
-			// console.log(response.data);
-			vm.photosArray = response.data.slice(0, 10);
-
-
-		};
-
+    /*---------------- get hundler ------------------*/
+    getAlbumsSuccessHandler =
+        function(response) {
+            vm.photosArray = response.data.slice(0, 10);
+        };
 
     /*---------------- get data by $http.get ------------------*/
-
-	imagestHttp.getAlbums().then(getAlbumsSuccess).catch(function(err) {
-		console.log(err);
-	})
-
+    imagestHttp.getAlbums().then(getAlbumsSuccessHandler).catch(function(err) {
+        console.log(err);
+    });
 
     /*---------------- get data by $resource ------------------*/
     imagesResource.get().$promise.then(function(user) {
-      vm.photosArray2 = user.slice(0, 10);
+        vm.photosArray2 = user.slice(0, 10);
     });
 
 });
 
-
-app.directive('ngPreview', function () {
+app.directive('ngPreview', function() {
     return {
-        link: function ($scope, element, attrs) {
-            element.bind('click', function () {
-                    var node = document.createElement("div");
-                    node.setAttribute('id', 'preview');
-                    node.setAttribute('onclick', 'this.remove()');
-                    var url = element.attr('url');
+        link: function($scope, element, attrs) {
+            element.bind('click', function() {
 
-                    var html = '<div id="preview-wrapper"><img src="'+url+'"    /></div>';
+                var node = document.createElement("div");
+                node.setAttribute('id', 'preview');
+                node.setAttribute('onclick', 'this.remove()');
+                var url = element.attr('url');
 
-                    node.innerHTML += html;
-                    document.getElementsByTagName("body")[0].appendChild(node); 
+                var html = '<div id="preview-wrapper"><img src="' + url + '"    /></div>';
+
+                node.innerHTML += html;
+                document.getElementsByTagName("body")[0].appendChild(node);
             });
         }
     };
